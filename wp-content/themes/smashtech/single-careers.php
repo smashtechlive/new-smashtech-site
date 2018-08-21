76769292686d77a1ae048f60f89cc8b9636d1ac1
Joshua Maynard <jmmaynard88@gmail.com>
2018-08-21 14:53:32 -0700

    page builder and career sections

---
+++ wp-content/themes/smashtech/single-careers.php
@@ -0,0 +1,72 @@
+
+<?php get_header(); ?>
+	<?php if (have_posts()): while (have_posts()) : the_post(); 
+		$application_link = get_field('application_link');
+	?>
+		<div class="careers-single">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col no-pad">
+						<div class="hero small" style="background: url(../../wp-content/themes/smashtech/img/career-top.jpg) no-repeat top center;">
+							<h1>MORE THAN A CAREER A LIFESTYLE</h1>
+						</div>
+					</div><!-- .col -->
+				</div><!-- .row -->
+			</div><!-- .container -->
+
+
+			<div class="container">
+					<div class="row post-header">
+						<div class="col">
+							<div class="bottom-border">
+								<div class="row">
+									<div class="col-sm-6">
+										<h2><?= the_title() ?></h2>
+										<p>Marketing - San Diego, California</p>
+									</div>
+
+									<div class="col-sm-6">
+										<p class="apply-now">
+											<a href="<?= $application_link ?>" class="btn btn-yellow" target="_blank">
+												Apply >
+											</a>
+										</p>
+									</div>
+								</div><!-- .row -->
+							</div><!-- .bottom-border -->
+						</div><!-- .col -->
+					</div><!-- .row -->
+
+				<div class="row">
+					<div class="col">
+						<?= the_content() ?>
+						<p><strong>Are we a fit? Click on the link below to apply!</strong></p>
+						<p><a href="<?= $application_link ?>" class="btn btn-yellow" target="_blank">apply ></a></p>
+					</div>
+				</div><!-- .row -->
+
+				<div class="row">
+					<div class="col">
+						SHARE
+					</div>
+				</div><!-- .row -->
+			
+
+			</div><!-- .container -->
+		</div><!-- .careers-single -->
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+					<!-- article -->
+		<article>
+
+			<h1><?php _e( 'Sorry, nothing to display.', 'smashtech' ); ?></h1>
+
+		</article>
+		<!-- /article -->
+
+	<?php endif; ?>
+
+
+<?php get_footer(); ?>
