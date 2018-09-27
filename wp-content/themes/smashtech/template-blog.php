bfe628ce337043ccd815b36ed9ffaac35ab8854a
Joshua Maynard <jmmaynard88@gmail.com>
2018-09-27 15:56:27 -0700

    links

--- wp-content/themes/smashtech/template-blog.php
+++ wp-content/themes/smashtech/template-blog.php
@@ -95,8 +95,10 @@
 				</div><!-- .row -->
 				<div class="row">
 					<div class="col-sm-6 mt-3 play-container" data-aos="fade-right">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/play_button.png" class="play-button">
-						<img src="<?= wp_get_attachment_url( get_post_thumbnail_id($smash_pick->ID) ) ?>" class="img-fluid">
+						<a href="<?= $smash_pick->guid ?>">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/play_button.png" class="play-button">
+							<img src="<?= wp_get_attachment_url( get_post_thumbnail_id($smash_pick->ID) ) ?>" class="img-fluid">
+						</a>
 					</div><!-- .col -->
 					<div class="col-sm-6 mt-3" data-aos="fade-left">
 						<h2><?= $smash_pick->post_title ?></h2>
