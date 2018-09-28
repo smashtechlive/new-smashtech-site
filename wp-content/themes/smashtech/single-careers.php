91d8347e23825f845aa3636b661d8c50c9896eda
Joshua Maynard <jmmaynard88@gmail.com>
2018-09-28 12:38:36 -0700

    qa fixes

--- wp-content/themes/smashtech/single-careers.php
+++ wp-content/themes/smashtech/single-careers.php
@@ -5,25 +5,38 @@
 	?>
 		<div class="careers-single">
 			<div class="container-fluid">
-					<div class="row">
-						<div class="col no-pad">
-							<div class="d-flex hero small" style="background: url(../../wp-content/themes/smashtech/img/careers_header.jpg) no-repeat top center;">
-								<div class="align-self-center justify-content-center">
-									<h1>MORE THAN A CAREER A LIFESTYLE</h1>
+				<? 
+				  $hero = get_field('hero');	
+					if ($hero): 
+						if (!$hero[image][url]) $hero[image][url] = '../../wp-content/themes/smashtech/img/careers_header.jpg';
+						if (!$hero[title_label]) $hero[title_label] = 'MORE THAN A CAREER A LIFESTYLE';
+				?>
+						<div class="row">
+							<div class="col no-pad">
+								<div class="d-flex hero small" style="background: url('<?= $hero[image][url] ?>') top center no-repeat;">
+									<div class="align-self-center justify-content-center">
+										<h1><?= $hero[title_label] ?></h1>
+										<p><?= $hero[paragraph_content] ?></p>
+										<i class="fa fa-chevron-down"></i>
+									</div>
+									
 								</div>
-							</div>
-						</div><!-- .col -->
-					</div><!-- .row -->
-				</div><!-- .container -->
 
-			<div class="container">
+							</div><!-- .col -->
+						</div><!-- .row -->
+					<? endif; ?>
+
+
+				</div><!-- .container -->
+				<? $category = get_the_category(); ?>
+				<div class="container">
 					<div class="row post-header">
 						<div class="col">
 							<div class="bottom-border">
 								<div class="row">
 									<div class="col-sm-6">
 										<h2><?= the_title() ?></h2>
-										<p>Marketing - San Diego, California</p>
+										<p><?= $category[0]->name ?> - San Diego, California</p>
 									</div>
 
 									<div class="col-sm-6">
